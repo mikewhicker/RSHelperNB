72c652b7e26afd4b3a363359a6d4ad7024d3f784
Mike Whicker <michael.j.whicker@sherwin.com>
2022-10-15 18:10:07 -0400

    Cleanup

--- routes/githubRoutes.js
+++ routes/githubRoutes.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const router = express.Router();
-//const app = express();
 const path = require('path');
 const pubFolder = path.resolve(__dirname, '../public');
 const dbFolder = path.resolve(__dirname, '../db');
 const db = require(dbFolder + '/rmdb.js');
-//const sqlite3 = require('sqlite3').verbose();
 
-
-
+//GitHub Page
 router.get('/', (req, res) => {
     res.sendFile(pubFolder + '/github.html');
 });
 
+//Get Repo Group Selector Data
 router.get('/grps', function (req, res) {
     db.getRepoGrpSel().then(data => {
         res.json(data);
